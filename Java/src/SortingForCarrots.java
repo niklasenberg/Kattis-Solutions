f59248a14038577aa78a3e96485f15fd56a0fdbb
Niklas Enberg <nickeen95@gmail.com>
2022-02-20 17:15:49 +0100

    Sorting For Carrots

---
+++ Java/src/SortingForCarrots.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SortingForCarrots {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String line = in.nextLine();
+
+        String[] numbers = line.split(" ");
+
+        System.out.println(numbers[1]);
+    }
+}
