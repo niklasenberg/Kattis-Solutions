30fe518a42aa22ab8a9e9f30ab12bf3221c54945
Niklas Enberg <nickeen95@gmail.com>
2022-02-20 17:02:51 +0100

    Stuck In A Time Loop

---
+++ Java/src/StuckInATimeLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StuckInATimeLoop {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+
+        for(int i = 1; i <= input; i++){
+            System.out.println(i + " Abracadabra");
+        }
+        
+    }
+}
