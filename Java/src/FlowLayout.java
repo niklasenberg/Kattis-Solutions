ef25b9926d801a2bcec167bd9aaf8d9f4bc3f84f
Niklas Enberg <nickeen95@gmail.com>
2022-03-16 09:35:45 +0100

    Fixed FlowLayout

--- Java/src/FlowLayout.java
+++ Java/src/FlowLayout.java
@@ -1,52 +1,54 @@
-//TODO: finish
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class FlowLayout {
     public static void main(String[] args) {
-        ArrayList<String> input = new ArrayList<>();
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        while(!line.equals("0")){
-            input.add(line);
-            line = in.nextLine();
-        }
 
-        HashMap<Integer, ArrayList<Pane>> paneMap = new HashMap<>();
-        int sequenceLength = input.size();
-        int maxWidth = 0;
-        int currentWidth = 0;
-        int currentHeight = 0;
-        for(int i = 0; i < sequenceLength; i++){
-            String[] numbers = input.get(i).split(" ");
-            if(numbers.length == 1){
-                maxWidth = Integer.parseInt(numbers[0]);
-            } else{
-                Pane pane = new Pane(Integer.parseInt(numbers[0]), Integer.parseInt((numbers[1])));
-                if(pane.width == -1){
-                    System.out.println(currentWidth + " x " + currentHeight);
-                    currentHeight = 0;
-                    currentWidth = 0;
-                    paneMap.clear();
-                }
+        int maxWidth = in.nextInt();
+        while (maxWidth != 0) {
+
+            //Establish layout with maximum of 15 rows
+            HashMap<Integer, int[]> paneMap = new HashMap<>();
+            for (int i = 0; i < 15; i++) {
+                paneMap.put(i, new int[2]);
             }
-        }
+
+            //Get new pane
+            int paneWidth = in.nextInt();
+            int paneHeight = in.nextInt();
+            while (paneWidth > 0) { //While input isnt -1 -1
+                for (int rowIndex : paneMap.keySet()) { //For each row
+                    int[] row = paneMap.get(rowIndex);
+                    if (row[0] + paneWidth <= maxWidth) { //Check whether pane fits in width
+                        if (rowIndex == 14 || paneMap.get(rowIndex + 1)[1] == 0) { //Check whether pane fits in height
+                            row[0] += paneWidth; //Add pane to row
+                            if (row[1] < paneHeight) {
+                                row[1] = paneHeight; //Update height if new pane is higher
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                //Get next pane
+                paneWidth = in.nextInt();
+                paneHeight = in.nextInt();
+            }
 
 
+            //Get final dimensions of layout
+            int mapWidth = 0;
+            int mapHeight = 0;
+            for (int rowIndex : paneMap.keySet()) {
+                if (paneMap.get(rowIndex)[0] > mapWidth) {
+                    mapWidth = paneMap.get(rowIndex)[0];
+                }
+                mapHeight += paneMap.get(rowIndex)[1];
+            }
 
-
-
-    }
-
-    private static class Pane{
-        private final int width;
-        private final int height;
-
-        public Pane(int width, int height) {
-            this.width = width;
-            this.height = height;
+            System.out.println(mapWidth + " x " + mapHeight);
+            maxWidth = in.nextInt();
         }
     }
 }
