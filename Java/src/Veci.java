4fc7ffdce683788b1f5fbbbe925ec2a5e535dff7
Niklas Enberg <nickeen95@gmail.com>
2022-03-18 23:21:08 +0100

    python + began Veci.java

---
+++ Java/src/Veci.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Veci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int original = in.nextInt();
+
+        ArrayList<Integer> combinations = new ArrayList<>();
+
+
+    }
+}
